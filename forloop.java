c99135340cb2bb5ea30080f5236bc003cb80238f
gatesikev <149663468+gatesikev@users.noreply.github.com>
2023-11-07 03:26:31 -0800

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,9 @@
+// A java program using For loop
+public class forloop{  
+public static void main(String[] args) {  
+    //Code of Java for loop  
+    for(int i=1;i<=10;i++){  
+        System.out.println(i);  
+    }  
+}  
+}  
