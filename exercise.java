57474e24a33cfda87d3c4c86b03fa3456195f421
gatesikev <149663468+gatesikev@users.noreply.github.com>
2023-11-07 03:43:06 -0800

    Update exercise.java

--- exercise.java
+++ exercise.java
@@ -1,5 +1,7 @@
+//This is a java program which displays the sum of 2 numbers
 public class exercise {
     public static void main(String[] args) {
+      // initialisation
         int a=12;
         int b=45;
         float sum;
