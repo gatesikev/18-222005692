abc2d7e38e33871551d316179d080a5beec8e440
gatesikev <149663468+gatesikev@users.noreply.github.com>
2023-11-07 02:48:31 -0800

    Add files via upload

---
+++ me.java
@@ -0,0 +1,17 @@
+// Java program which uses if else ladder
+    public class me {    
+        public static void main(String[] args) {    
+            int number=15;    
+            if(number>0){  
+            System.out.println("POSITIVE");  
+            }
+                else if(number<0){  
+            System.out.println("NEGATIVE");  
+            }
+            
+            else{  
+            System.out.println("ZERO");  
+           }  
+        }    
+        }    
+
