f97bd5860406152eb962e4173bb85f6ef917f70f
gatesikev <149663468+gatesikev@users.noreply.github.com>
2023-11-07 03:37:32 -0800

    Add files via upload

---
+++ exercise.java
@@ -0,0 +1,11 @@
+public class exercise {
+    public static void main(String[] args) {
+        int a=12;
+        int b=45;
+        float sum;
+        sum= a+b;
+        System.out.println("The sum of 2 numbers is:"+ sum);
+        
+
+    }
+}
