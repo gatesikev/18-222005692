9e38bc181bdccd92b91cafe457c90491588ad42c
gatesikev <149663468+gatesikev@users.noreply.github.com>
2023-11-07 03:25:00 -0800

    Add files via upload

---
+++ ifexample.java
@@ -0,0 +1,12 @@
+//Java Program to demonstate the use of if statement.  
+public class ifexample {  
+    public static void main(String[] args) {  
+        //defining an 'age' variable  
+        int age=18;  
+        //checking the age  
+        if(age>=18){  
+            System.out.print("You're an adult");  
+        }  
+    }  
+    }  
+
